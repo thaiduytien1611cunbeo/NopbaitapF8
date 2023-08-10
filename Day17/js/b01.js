23c9a26c745687dd4c2cbcba72b85f93d71d8565
thaiduytien1611cunbeo <thaiduytien16112004@gmail.com>
2023-08-11 01:51:31 +0700

    Nop bai tap

--- Day17/js/b01.js
+++ Day17/js/b01.js
@@ -1,130 +1,181 @@
-// Bai 01 =====================================================
+// Bai 01 ==================================================================
+// N Số FIBONACI ===========================================================
 
-var distance = 10;
-var money = 0;
+// function checkFibo(n) {
+//     if(n === 1 || n === 2) {
+//         return 1;
+//     }
+//     return checkFibo(n - 1) + checkFibo(n - 2);
+// }
 
-if (distance < 0) {
-    console.log('Nhập lại');
-} else if (distance <= 1) {
-    money = 15000;
-} else if (distance <= 5) {
-    money = distance * 13500;
-} else if (distance <= 120) {
-    money = distance * 11000;
-} else {
-    money = distance * 11000 / 10;
-}
+// var count = 1; 
 
-console.log(`Tien TAXI : ${money}`);
+//  function listFibo(n) {
 
-// Bai 02 ======================================================
-
-var numberKwh = 100;
-var money = 0;
-
-if (numberKwh <= 0) {
-    console.log('Nhập lại');
-} else {
-    for (var i = 1; i <= numberKwh; i++) {
-        if (i <= 50) {
-            money += 1678;
-        }else if (i <= 100) {
-            money += 1374;
-        }else if (i <= 200) {
-            money += 2014;
-        }else if (i <= 300) {
-            money += 2536;
-        }else if (i <= 400) {
-            money += 2834;
-        }else {
-            money += 2927;
-        }
-    }
-}
-
-
-// console.log(`Tiền Điện = ${money}`);
-
-// Bai 03 ====================================================
-
-var n = 3;
-var s = 0;
-for (var i = 1; i <= n; i++) {
-    s += i * (i + 1);
-}
-
-console.log(`S = ${s}`);
-
-
-// Bai 04 =====================================================
-
-function prime(n) {
-    for (var i = 2; i <=Math.sqrt(n); i++) {
-        if (n % i === 0) return false;
-    } return n > 1;
-}
-
-var a = 1;
-
-if (prime(a)) {
-    console.log(`a is prime`);
-} else {
-    console.log(`a is not prime`);
-}
-
-// Bai 05 =======================================================
-
-var row = 5;
-var count = 1;
-
-for (var i = 1; i <= row; i++) {
-    var temp = "";
-    for (var  j = 1; j <=i; j++) {
-        if (j === i) {
-            temp += `${count++}`;
-        } else {
-            temp += `${count++} `;
-        }        
-    }
-    console.log(temp);
-}
-
-// Bài 06 ===================================================
+//     if(count > n) { 
+//         return false;
+//     } else {
+//         console.log(checkFibo(count));  
+//         count++;
+//         return listFibo(n);  
+//     }
     
-    for (var i = 1; i <= 8; i++) {
-        var temp = "";
-        for (var j = 1; j <= 8; j++) {
-            var ans ;
-            if (i * j % 2 === 0) {
-                ans = 'black';
-            }else {
-                ans = 'white';
-            }
-            temp += `${ans} `;
-        }
-        console.log(temp);
-    }
+//  }
 
-// Bài 07 ===================================================
-    
-    for (var i = 1; i <= 10; i++) {
-        for (var j = 1; j <= 10; j++) {
-            var temp = "";
-            var ans = i * j;
-            temp += `${i} * ${j} = ${ans}`;
-            console.log(temp);
-        }
-    }
+//  listFibo(10);
 
-// Bài 08 ===================================================
+// Bai 02 ========================================================================
+// Đảo ngược số ==================================================================
 
-function total(n) {
-    if (n > 1) {
-        return 1/n + total(n-1); 
-    }
-    return 1;
-}
+//  function reverse(n) {
+//     var ans = 0;
+//     while(n > 0) {
+//         var temp = n % 10;
+//         temp = Math.trunc(temp);
+//         ans = (ans * 10) + temp;
+//         n /= 10;
+//         n = Math.trunc(n);
+//     }
+//     console.log(ans);
+//  }   
 
-var n = 3;
+// reverse(2345)
 
-console.log(total(n));
+// Bai 03 ========================================================================================
+// Viết hàm chuyển số thành chữ ==================================================================
+
+// function lowerString(n) {
+//     switch(n) {
+//         case 0:
+//             return 'không'; 
+//         case 1:
+//             return 'một';
+//         case 2:
+//             return 'hai';
+//         case 3:
+//             return 'ba';
+//         case 4:
+//             return 'bốn';
+//         case 5:
+//             return 'năm'; 
+//         case 6:
+//             return 'sáu';
+//         case 7:
+//             return 'bảy';
+//         case 8:
+//             return 'tám';
+//         case 9:
+//             return 'chín';
+//         default:
+//             console.log(false);
+//     }
+// }
+
+// function upperString(n) {
+//     switch(n) {
+//         case 0:
+//             return 'Không'; 
+//         case 1:
+//             return 'Một';
+//         case 2:
+//             return 'Hai';
+//         case 3:
+//             return 'Ba';
+//         case 4:
+//             return 'Bốn';
+//         case 5:
+//             return 'Năm'; 
+//         case 6:
+//             return 'Sáu';
+//         case 7:
+//             return 'Bảy';
+//         case 8:
+//             return 'Tám';
+//         case 9:
+//             return 'Chín';
+//         default:
+//             console.log(false);
+//     }
+// }
+
+//  function toString(n) {
+//     var ans = "", a, b, c, d;
+//     if (n >= 0 && n <= 9999) {
+//         // tách các số hàng nghìn, hàng trăm, hàng chục gán bằng a, b, c, d;
+//         a = n / 1000; 
+//         a = Math.trunc(a); 
+//         n -= 1000 * a;
+//         b = n / 100; 
+//         b = Math.trunc(b);
+//         n -= 100 * b;
+//         c = n / 10; 
+//         c= Math.trunc(c);
+//         n -= 10 * c;
+//         d = n ; 
+//         d = Math.trunc(d);
+
+//         if (a === 0 && b === 0 && c === 0) {
+//             ans = lowerString(d);
+//         }
+//         else {
+//             // check trường hợp của a và b
+//         a = a === 0 ? "" : `${upperString(a)} nghìn `;
+//         b = b === 0 ? "" : `${lowerString(b)} trăm `;
+        
+//         // check trường hợp đặc biệt
+//         if (b === "" && d !== 0) {
+//             b = `không trăm `;
+//         }
+
+//         // check trường hợp của c và d
+//         if (d === 0) {
+//             d = "";
+//             if (c === 0) {
+//                 c = "";
+//             } else if (c === 1) {
+//                 c = "mười";
+//             } else {
+//                 c = `${lowerString(c)} mươi`
+//             }
+//         } else if (d === 1) {
+//             d = "";
+//             if (c === 0) {
+//                 c = "lẻ một";
+//             } else if (c === 1) {
+//                 c = "mười một";
+//             } else {
+//                 c = `${lowerString(c)} mươi mốt`
+//             }
+//         } else if (d === 5) {
+//             d = `lăm`;
+//             if (c === 0) {
+//                 c = "lẻ ";
+//             } else if (c === 1) {
+//                 c = "mười ";
+//             } else {
+//                 c = `${lowerString(c)} mươi `
+//             }
+//         } else {
+//             d = `${lowerString(d)}`;
+//             if (c === 0) {
+//                 c = "lẻ ";
+//             } else if (c === 1) {
+//                 c = "mười ";
+//             } else {
+//                 c = `${lowerString(c)} mươi `
+//             }
+//         } 
+//         ans = `${a}${b}${c}${d}`
+//     }
+
+//         console.log(ans);
+//     }
+//     else {
+//         console.log('Nhập số khác hộ em');
+//     }
+//  }
+
+//  // NHẬP TEST CASE VÀO ĐÂY NHÉ ANH
+ 
+//   toString(111)
+   
