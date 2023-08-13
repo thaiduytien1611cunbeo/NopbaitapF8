724828dc2e12bd605cab621b43bd72eb87fed407
thaiduytien1611cunbeo <thaiduytien16112004@gmail.com>
2023-08-13 14:11:23 +0700

    Nop bai tap

---
+++ Day19/js/b01.js
@@ -0,0 +1,27 @@
+var courses = [
+    {
+        id: 1,
+        name: 'Javascript',
+        coin: 250
+    },
+    {
+        id: 2,
+        name: 'Html',
+        coin: 250
+    },
+    {
+        id: 3,
+        name: 'CSS',
+        coin: 250
+    },
+    {
+        id: 4,
+        name: 'Ruby',
+        coin: 50
+    },
+    {
+        id: 5,
+        name: 'PHP',
+        coin: 0
+    },
+]
