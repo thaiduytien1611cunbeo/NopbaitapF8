35b009ffcfb5f8d467e89a048af13ea9c0136981
thaiduytien1611cunbeo <thaiduytien16112004@gmail.com>
2023-10-08 18:24:44 +0700

    Nop bai tap

--- Day32/js/script.js
+++ Day32/js/script.js
@@ -1,33 +1,37 @@
 const counter = document.querySelector(".counter");
+counter.innerText = 2;
 const btn = document.querySelector(".btn");
 
 let startTime;
 let cnt = 0;
 
 const timer = function (currentTime) {
-  if (!startTime)  startTime = currentTime;
+    if (!startTime)  startTime = currentTime;
 
-  const elapsed = currentTime - startTime;
-    
+    const elapsed = currentTime - startTime;
+        
 
-  if (elapsed >= 1000) {
-    counter.textContent--;
-    startTime = currentTime;
-  };
+    if (elapsed >= 1000) {
+        counter.textContent--;
+        startTime = currentTime;
+    };
 
-  if(+counter.textContent > 0) {
-    requestAnimationFrame(timer);
-    cnt++;
-    console.log(cnt);
-  }
-  else btn.classList.add("action");
-
+    if(+counter.textContent > 0) {
+        requestAnimationFrame(timer);
+        cnt++;
+    }
+    else checkClick(btn);
 };
 
 requestAnimationFrame(timer);
 
-btn.addEventListener("click", function() {
+function checkClick(btn) {
     if (+counter.textContent === 0) {
-        window.location.href='https://fullstack.edu.vn'
+        btn.removeAttribute('disabled'); 
+        btn.classList.add("action");
+        btn.addEventListener("click", function() {
+            window.location.href='https://fullstack.edu.vn'; 
+        });
     };
-});
+}
+
