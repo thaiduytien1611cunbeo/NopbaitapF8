9827c8e1a1ea3e9da72d723cfdff2cbfeeab3fa6
thaiduytien1611cunbeo <thaiduytien16112004@gmail.com>
2023-10-13 15:55:34 +0700

    Nop bai tap

--- Day34/js/script.js
+++ Day34/js/script.js
@@ -76,9 +76,7 @@ const handlerCurrentValue = (currentValue) => {
             break;
 
 
-        case currentValue.includes('tới vinhomes smart city') ? currentValue : undefined : 
-            window.open('https://www.google.com/maps/search/vinhomes+smart+city+t%C3%A2y+m%E1%BB%97/@21.0041336,105.745652,16z/data=!3m1!4b1?entry=ttu')
-            break;
+        
         case currentValue.includes('mở video') ? currentValue : undefined :
             var value = currentValue.slice(('mở video').length).trim();
             window.open(`https://www.youtube.com/results?search_query=${value}`)
@@ -86,6 +84,9 @@ const handlerCurrentValue = (currentValue) => {
         case currentValue.includes('mở bài hát') ? currentValue : undefined :
             var value = currentValue.slice(('mở bài hát').length).trim();
             window.open(`https://zingmp3.vn/tim-kiem/tat-ca?q=${value}`)
+            break;
+        case currentValue.includes('chỉ', '')||currentValue.includes('đường', '')||currentValue.includes('tới', '') ? currentValue : undefined : 
+            window.open(`https://www.google.com/maps/search/${currentValue.replaceAll('chỉ', '').replaceAll('đường', '').replaceAll('tới', '')}+t%C3%A2y+m%E1%BB%97/@21.0041336,105.745652,16z/data=!3m1!4b1?entry=ttu`)
             break;
 
         default:
